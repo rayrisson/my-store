aaee0dffe1318f9839ae7f247342b4b4434b18ee
Rayrisson <rayrissonvinicius@gmail.com>
2024-07-08 08:33:39 -0300

    Page without suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import FilterSection from '@/core/components/FilterSection'
 import Header from '@/core/components/Header'
 import ProductList from '@/core/components/ProductList'
-import { ProductListSkeleton } from '@/core/components/Skeleton'
 import { objectToSearchParamString } from '@/core/utils/searchParamsUtils'
-import { Suspense } from 'react'
 import styles from './page.module.css'
 
 export default async function Home({
@@ -16,10 +14,8 @@ export default async function Home({
     <>
       <Header />
       <div className={styles.mainWrapper}>
-        <Suspense key={query} fallback={<ProductListSkeleton />}>
-          <FilterSection />
-          <ProductList query={query} />
-        </Suspense>
+        <FilterSection />
+        <ProductList query={query} />
       </div>
     </>
   )
